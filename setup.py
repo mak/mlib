8ce949827106968c7521fce284c014f039c72392
mak <mak@lokalhost.pl>
2017-12-18 13:49:29 +0100

    prepare for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python2
 from setuptools import setup
 
+
+def long_description():
+    with open('README.rst') as f:
+        d=f.read()
+    return d
+
 setup(name='mLib',
       version='1.2.1',
       description='Library contains functions commonly used in malware research',
+      long_description=long_description(),
       author='Maciej Kotowicz',
       author_email='mak@lokalhost.pl',
       url='https://github.com/mak/mlib',
       package_dir={'mlib': 'src'},
       packages=['mlib', 'mlib.compression', 'mlib.crypto',
                 'mlib.disasm', 'mlib.winapi', 'mlib.malware'],
-      package_data={'mlib': ['so/*so']}
-      )
+      package_data={'mlib': ['so/*so']},
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: Public Domain',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2 :: Only',
+          'Programming Language :: Assembly',
+          'Programming Language :: C',
+          'Topic :: Utilities'
+      ])
