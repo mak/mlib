a703ff4c5f34b617e283d31b8bbc0145178c3596
mak <mak@lokalhost.pl>
2017-12-18 14:50:35 +0100

    merge with gh

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def long_description():
     return d
 
 setup(name='mLib',
-      version='1.2.1',
+      version='1.2.0',
       description='Library contains functions commonly used in malware research',
       long_description=long_description(),
       author='Maciej Kotowicz',
