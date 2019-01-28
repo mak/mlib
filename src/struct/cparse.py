b626e8d54b89caa85cc2ac2f6513ff09f90ec757
mak <mak@lokalhost.pl>
2019-01-28 07:57:50 +0100

    fix short ints

--- src/struct/cparse.py
+++ src/struct/cparse.py
@@ -7,8 +7,8 @@ PREPEND_TYPES = '''
 typedef unsigned short wchar_t;
 typedef signed char __int8_t;
 typedef unsigned char __uint8_t;
-typedef signed short int __int16_t;
-typedef unsigned short int __uint16_t;
+typedef signed short  __int16_t;
+typedef unsigned short  __uint16_t;
 typedef signed int __int32_t;
 typedef unsigned int __uint32_t;
 typedef signed long long int __int64_t;
