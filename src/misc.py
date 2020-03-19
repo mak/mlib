853639e75df8360f8577c2f4ecfe3cfefa8dc998
mak <mak@lokalhost.pl>
2020-03-19 22:10:58 +0100

    misc: hexdump

--- src/misc.py
+++ src/misc.py
@@ -65,6 +65,16 @@ class E(enum.Enum):
 #         print _data
 #         raise Exception('asdf')
 
+def hexdump(src, length=16):
+    FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
+    lines = []
+    for c in xrange(0, len(src), length):
+        chars = src[c:c+length]
+        hex = ' '.join(["%02x" % ord(x) for x in chars])
+        printable = ''.join(["%s" % ((ord(x) <= 127 and FILTER[ord(x)]) or '.') for x in chars])
+        lines.append("%04x  %-*s  %s\n" % (c, length*3, hex, printable))
+    return ''.join(lines)
+
 
 def generic_unparse(data, do_rest=False):
     cfg = deepcopy(data)
